c261b8fd8df2b7d45af53623528abf1ea958a7df
zacanger <zac@zacanger.com>
2016-01-28 09:48:17 -0700

    updating this branch, plan to merge in
    npm-scripts totally all fixed now.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 var spawn = require('child_process').spawn
-  , args  = ['-r', '-f', '.']
+  , args  = ['-r', '.']
 args = args.concat(process.argv.slice(2))
 spawn('./node_modules/.bin/nwbuild', args, {cwd: __dirname, stdio: [0, 1, 2]})
 

13673ad3977da6f1a7d0776425a2183f29c7efc9
zacanger <zac@zacanger.com>
2016-02-01 12:02:48 -0700

    readitor.jsx

--- src/components/app/readitor/Readitor.jsx
+++ src/components/app/readitor/Readitor.jsx
@@ -8,18 +8,18 @@ import Tree      from './tree/FileTree.jsx'
 const stylesheet = require('!css!less!./readitor.less').toString()
 
 const BASEREF = 'https://pharaohjs.firebaseio.com/session/'
-
-const cmConfig = {
-  lineWrapping      : true
-, mode              : 'javascript'
-, theme             : 'abcdef'
-, lineNumbers       : true
-, matchBrackets     : true
-, lineWrapping      : true
-, autoCloseBrackets : true
-, autoCloseTags     : true
-}
-
+//
+// const cmConfig = {
+//   lineWrapping      : true
+// , mode              : 'javascript'
+// , theme             : 'abcdef'
+// , lineNumbers       : true
+// , matchBrackets     : true
+// , lineWrapping      : true
+// , autoCloseBrackets : true
+// , autoCloseTags     : true
+// }
+//
 const themeNames = [
   'default'
 , 'monokai'

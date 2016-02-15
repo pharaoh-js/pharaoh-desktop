2dca8a7d8fe94d2e8a1f254babbd8651ec9a024e
zacanger <zac@zacanger.com>
2016-02-14 22:54:30 -0700

    just a slight update to some nonsense

--- index.js
+++ index.js
@@ -4,4 +4,3 @@ var spawn = require('child_process').spawn
   , args  = ['-r', '.']
 args = args.concat(process.argv.slice(2))
 spawn('./node_modules/.bin/nwbuild', args, {cwd: __dirname, stdio: [0, 1, 2]})
-

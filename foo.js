d32c84201ac9e03629aec17c7097e4cdf4872704
zacanger <zac@zacanger.com>
2016-11-12 12:33:09 -0700

    remove crap

--- foo.js
+++
@@ -1,6 +0,0 @@
-makeFirePad (subRef, config) {
-    let fpRef = new Firebase(BASEREF + subRef);
-    let codeMirror = CodeMirror(document.getElementById('pad'), config);
-    this.firepad = Firepad.fromCodeMirror(fpRef, codeMirror, // this is the line we're having trouble with
-      { defaultText: 'Hello Firepad!!!!' });
-  },
